98a4b11e9cf4254a03d04a1a332cdda11ed2a6b4
bio-25 <kunalnandwana1999@gmail.com>
2020-10-22 20:14:12 +0530

    oiu

--- js/index.js
+++ js/index.js
@@ -1,253 +1,82 @@
 
-
-$(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 100) {
       $(".navbar").css("background" , "white");
       $(".nav-link").css("color" , "black");
+      console.log('hdu');
 	  }
-
 	  else{
       $(".navbar").css("background" , "transparent" );  
-      $(".nav-link").css("color" , "white");	
+      $(".nav-link").css("color" , "black");	
 	  }
   })
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
-    console.log('dfsnj');
+    // $("a").on('click', function(event) {
 
-    var $container 	= $('.iso-box-wrapper'), 
-      $imgs 		= $('.iso-box img');
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //     // Prevent default anchor click behavior
+    //     event.preventDefault();
+
+    //     // Store hash
+    //     var hash = this.hash;
+
+    //     // Using jQuery's animate() method to add smooth page scroll
+    //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //     $('html, body').animate({
+    //         scrollTop: $(hash).offset().top
+    //     }, 800, function(){
+
+    //         // Add hash (#) to URL when done scrolling (default click behavior)
+    //         window.location.hash = hash;
+    //     });
+    //     } // End if
+    // });
+    
 
 
 
-    $container.imagesLoaded(function () {
-
-      $container.isotope({
-      layoutMode: 'fitRows',
-      itemSelector: '.iso-box'
-      });
-
-      $imgs.load(function(){
-        $container.isotope('reLayout');
-      })
-
-    });
-
-    //filter items on button click
-
-    $('.filter-wrapper li a').click(function(){
-
-        var $this = $(this), filterValue = $this.attr('data-filter');
-
-    $container.isotope({ 
-      filter: filterValue,
-      animationOptions: { 
-          duration: 750, 
-          easing: 'linear', 
-          queue: false, 
-      }              	 
-    });	            
-
-    // don't proceed if already selected 
-
-    if ( $this.hasClass('selected') ) { 
-      return false; 
-    }
-
-    var filter_wrapper = $this.closest('.filter-wrapper');
-    filter_wrapper.find('.selected').removeClass('selected');
-    $this.addClass('selected');
-
-      return false;
-    }); 
-
-}
-
-});
-    $("a").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-
-        // Store hash
-        var hash = this.hash;
-
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
-        } // End if
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: '2',
+      // coverflowEffect: {
+      //   rotate: 50,
+      //   stretch: 0,
+      //   depth: 100,
+      //   modifier: 1,
+      //   slideShadows : true,
+      // },
+      
+      coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 50,
+          modifier: 6,
+          slideShadows : false,
+        },
+        
     });
     
-
-$('.navbar-collapse a').click(function(){
-  $(".navbar-collapse").collapse('hide');
-});
-
-
-(function() {
-
-    // VARIABLES
-    const timeline = document.querySelector(".timeline ol"),
-      elH = document.querySelectorAll(".timeline li > div"),
-      arrows = document.querySelectorAll(".timeline .arrows .arrow"),
-      arrowPrev = document.querySelector(".timeline .arrows .arrow__prev"),
-      arrowNext = document.querySelector(".timeline .arrows .arrow__next"),
-      firstItem = document.querySelector(".timeline li:first-child"),
-      lastItem = document.querySelector(".timeline li:last-child"),
-      xScrolling = 280,
-      disabledClass = "disabled";
     
-    // START
-    window.addEventListener("load", init);
-    
-    function init() {
-      setEqualHeights(elH);
-      animateTl(xScrolling, arrows, timeline);
-      setSwipeFn(timeline, arrowPrev, arrowNext);
-      setKeyboardFn(arrowPrev, arrowNext);
-    }
-    
-    // SET EQUAL HEIGHTS
-    function setEqualHeights(el) {
-      let counter = 0;
-      for (let i = 0; i < el.length; i++) {
-        const singleHeight = el[i].offsetHeight;
-    
-        if (counter < singleHeight) {
-          counter = singleHeight;
+  var galleryTop = new Swiper('.swiper-container.testimonial', {
+      speed: 400,
+      spaceBetween: 50,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      direction: 'vertical',
+      pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      thumbs: {
+          swiper: galleryThumbs
         }
-      }
-    
-      for (let i = 0; i < el.length; i++) {
-        el[i].style.height = `${counter}px`;
-      }
-    }
-    
-    // CHECK IF AN ELEMENT IS IN VIEWPORT
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-    
-    // SET STATE OF PREV/NEXT ARROWS
-    function setBtnState(el, flag ) {
-      if (flag) {
-        el.classList.add(disabledClass);
-      } else {
-        if (el.classList.contains(disabledClass)) {
-          el.classList.remove(disabledClass);
-        }
-        el.disabled = false;
-      }
-    }
-    
-    // ANIMATE TIMELINE
-    function animateTl(scrolling, el, tl) {
-      let counter = 0;
-      for (let i = 0; i < el.length; i++) {
-        el[i].addEventListener("click", function() {
-          if (!arrowPrev.disabled) {
-            arrowPrev.disabled = true;
-          }
-          if (!arrowNext.disabled) {
-            arrowNext.disabled = true;
-          }
-          const sign = (this.classList.contains("arrow__prev")) ? "" : "-";
-          if (counter === 0) {
-            tl.style.transform = `translateX(-${scrolling}px)`;
-          } else {
-            const tlStyle = getComputedStyle(tl);
-            // add more browser prefixes if needed here
-            const tlTransform = tlStyle.getPropertyValue("-webkit-transform") || tlStyle.getPropertyValue("transform");
-            const values = parseInt(tlTransform.split(",")[4]) + parseInt(`${sign}${scrolling}`);
-            tl.style.transform = `translateX(${values}px)`;
-          }
-    
-          setTimeout(() => {
-            isElementInViewport(firstItem) ? setBtnState(arrowPrev) : setBtnState(arrowPrev, false);
-            isElementInViewport(lastItem) ? setBtnState(arrowNext) : setBtnState(arrowNext, false);
-          }, 1100);
-    
-          counter++;
-        });
-      }
-    }
-    
-    // ADD SWIPE SUPPORT FOR TOUCH DEVICES
-    function setSwipeFn(tl, prev, next) {
-      const hammer = new Hammer(tl);
-      hammer.on("swipeleft", () => next.click());
-      hammer.on("swiperight", () => prev.click());
-    }
-    
-    // ADD BASIC KEYBOARD FUNCTIONALITY
-    function setKeyboardFn(prev, next) {
-      document.addEventListener("keydown", (e) => {
-        if ((e.which === 37) || (e.which === 39)) {
-          const timelineOfTop = timeline.offsetTop;
-          const y = window.pageYOffset;
-          if (timelineOfTop !== y) {
-            window.scrollTo(0, timelineOfTop);
-          }
-          if (e.which === 37) {
-            prev.click();
-          } else if (e.which === 39) {
-            next.click();
-          }
-        }
-      });
-    }
-    
     });
-
-
-
-
-
-
-
-$('.ma_link').click(function($e) {
-  $e.preventDefault();
-});
-
-
-$('.testimonial-carousel').slick({
-  dots: false,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: true,
-  prevArrow: '<button class="slick-prev slick-arrow transition" type="button" ><i class="fas fa-angle-left"></i></button>',
-  nextArrow: '<button class="slick-next slick-arrow transition" type="button" ><i class="fas fa-angle-right"></i></button>',
-  infinite: true,
-  responsive: [{
-      breakpoint: 1400,
-      settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-      }
-  }, {
-      breakpoint: 800,
-      settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-      }
-  }]
-});
+    
+  
