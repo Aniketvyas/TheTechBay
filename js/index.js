eb5cb7fb9f6f17cb0cec60ee7f2806e4901a9315
bio-25 <kunalnandwana1999@gmail.com>
2020-11-03 18:03:26 +0530

    final look animated

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 100) {
-        $(".navbar").css("background", "white");
-        $(".nav-link").css("color", "black");
-        console.log('hdu');
-    } else {
-        $(".navbar").css("background", "transparent");
-        $(".nav-link").css("color", "black");
-    }
-})
-
-
-
-
 var galleryThumbs = new Swiper('.gallery-thumbs', {
     effect: 'coverflow',
     grabCursor: true,
@@ -59,4 +44,29 @@ var galleryTop = new Swiper('.swiper-container.testimonial', {
 
 $('.navbar-collapse a').click(function() {
     $(".navbar-collapse").collapse('hide');
+});
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
