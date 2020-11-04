90c470fedb73f5a5ad12a0b66c678a29ff43dd9d
bio-25 <kunalnandwana1999@gmail.com>
2020-11-04 23:29:59 +0530

    roeinjg

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: '2',
+    slidesPerView: '1',
     // coverflowEffect: {
     //   rotate: 50,
     //   stretch: 0,
